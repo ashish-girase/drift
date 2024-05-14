e7040eff3cda4cff41d958737b5454d443ff873e
Khushali-25 <khushali@veravalonline.com>
2024-05-14 12:46:23 +0530

    change

--- config/database.php
+++ config/database.php
@@ -4,144 +4,106 @@ use Illuminate\Support\Str;
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Default Database Connection Name
+|--------------------------------------------------------------------------
+|
+| Here you may specify which of the database connections below you wish
+| to use as your default connection for all database work. Of course
+| you may use many connections at once using the Database library.
+|
+*/
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+'default' => env('DB_CONNECTION', 'mysql'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Database Connections
+|--------------------------------------------------------------------------
+|
+| Here are each of the database connections setup for your application.
+| Of course, examples of configuring each database platform that is
+| supported by Laravel is shown below to make development simple.
+|
+|
+| All database work in Laravel is done through the PHP PDO facilities
+| so make sure you have the driver for your particular database of
+| choice installed on your machine before you begin development.
+|
+*/
 
-    'connections' => [
+'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
+'mongodb' => [
+'driver' => 'mongodb',
+// 'dsn' => env('DB_URI', 'mongodb://10.188.4.129:27017/WindsonDispatch?readPreference=primary&directConnection=true&ssl=false'), //server
+// 'dsn' => env('DB_URI', 'mongodb+srv://astraportal:astraportal@astra.7uwteaq.mongodb.net/'), //server
+'dsn' => env('DB_URI', 'mongodb://neha:TetMJAXZ2VH35A5y@ac-8a170vo-shard-00-00.hjx5ms5.mongodb.net:27017,ac-8a170vo-shard-00-01.hjx5ms5.mongodb.net:27017,ac-8a170vo-shard-00-02.hjx5ms5.mongodb.net:27017/?replicaSet=atlas-z7c51e-shard-0&ssl=true&authSource=admin'), //server
+// 'dsn' => env('DB_URI', 'mongodb+srv://astraportal:astraportal@astra.7uwteaq.mongodb.net/'), //server
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+// 'dsn' => env('DB_URI', 'mongodb://appUser:appPassword@103.102.57.94:27017/WindsonDispatch?readPreference=primary&directConnection=true&ssl=false'), //dharmaj
+// 'dsn' => env('DB_URI', 'mongodb://astra:Astravo@203.88.144.78:27017/?authMechanism=SCRAM-SHA-1'),
+// 'dsn' => env('DB_URI', 'mongodb://172.31.89.128:27017'), //local
+// 'dsn' => env('DB_URI', 'mongodb://192.168.0.10:27017/WindsonDispatch?readPreference=primary&directConnection=true&ssl=false'), //dharmaj
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+'database' => 'ERP_Drift',
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
+]
 
-    ],
+],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Migration Repository Table
+|--------------------------------------------------------------------------
+|
+| This table keeps track of all the migrations that have already run for
+| your application. Using this information, we can determine which of
+| the migrations on disk haven't actually been run in the database.
+|
+*/
 
-    'migrations' => 'migrations',
+'migrations' => 'migrations',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Redis Databases
+|--------------------------------------------------------------------------
+|
+| Redis is an open source, fast, and advanced key-value store that also
+| provides a richer body of commands than a typical key-value system
+| such as APC or Memcached. Laravel makes it easy to dig right in.
+|
+*/
 
-    'redis' => [
+'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
+'options' => [
+'cluster' => env('REDIS_CLUSTER', 'redis'),
+'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
+'default' => [
+'url' => env('REDIS_URL'),
+'host' => env('REDIS_HOST', '127.0.0.1'),
+'username' => env('REDIS_USERNAME'),
+'password' => env('REDIS_PASSWORD'),
+'port' => env('REDIS_PORT', '6379'),
+'database' => env('REDIS_DB', '0'),
+],
 
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-        ],
+'cache' => [
+'url' => env('REDIS_URL'),
+'host' => env('REDIS_HOST', '127.0.0.1'),
+'username' => env('REDIS_USERNAME'),
+'password' => env('REDIS_PASSWORD'),
+'port' => env('REDIS_PORT', '6379'),
+'database' => env('REDIS_CACHE_DB', '1'),
+],
 
-    ],
+],
 
 ];
